fb4b71ff10653f5312eaae05f4147b1e18178892
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-06-22 20:27:06 -0300

    Add routes folder

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const app = express()
 
 app.use(logger('dev'))
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome to Tactical Table')
-// })
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
     console.log(`Express app listening on port ${ port }`)
