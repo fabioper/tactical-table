dc65761e80b57c92b468806f46aedf87f28af063
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-09-02 14:56:01 -0300

    Fix Promise.all

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 beforeEach(done => {
     const { teams, players } = mongoose.connection.collections;
 
-    Promise.all(teams.drop(), players.drop())
+    Promise.all([teams.drop(), players.drop()])
         .then(() => done())
         .catch(() => done());
 });
