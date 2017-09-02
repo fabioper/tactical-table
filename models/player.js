dd830c9939d78af3e40c229d4c5928aef993c482
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-09-02 14:37:32 -0300

    Setup player model

---
+++ models/player.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Team = require('./team');
+
+const PlayerSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    nickName: String,
+    birthDate: Date,
+    birthPlace: String,
+    nationality: String,
+    position: String,
+    currentClub: {
+        type: Schema.Types.ObjectId,
+        ref: 'team'
+    },
+    footStatsId: String
+});
+
+const Player = mongoose.model('player', PlayerSchema);
+
+module.exports = Player;
