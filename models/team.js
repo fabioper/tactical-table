0a1a859ab2b25d5a18fc5a7dbb5560b0947d4b48
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-09-02 13:55:34 -0300

    Set up Team model

---
+++ models/team.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TeamSchema = new Schema({
+    fullName: { type: String, required: true },
+    foundation: Date,
+    city: String,
+    country: String,
+    image: String
+});
+
+const Team = mongoose.model('team', TeamSchema);
+
+module.exports = Team;
