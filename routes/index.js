7cd671c013e910e02e93f23a091e9d59e69aa4ea
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-09-02 15:00:32 -0300

    Setup mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const config = require('../config').get(process.env.NODE_ENV);
+const mongoose = require('mongoose');
 const router = require('express').Router();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, { useMongoClient: true });
 
 router.use('/teams', require('./teams'));
 
