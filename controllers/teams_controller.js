ad62dd266318492f74b4c236b21dfe6796cba86a
Fábio Pereira <fabiopereira.fps@gmail.com>
2017-09-06 02:03:13 -0300

    Refactor teams_controller

--- controllers/teams_controller.js
+++ controllers/teams_controller.js
@@ -1,40 +1,40 @@
 const Team = require('../models/team')
 
-const index = (req, res) => {
-    const { sort, fields, limit, skip } = req.queries
+module.exports = {
+    index(req, res) {
+        const { sort, fields, limit, skip } = req.queries
 
-    Team.find()
-        .sort(sort)
-        .select(fields)
-        .limit(limit)
-        .skip(skip)
-        .then(teams => res.json({ results: teams }))
-        .catch(err => {
-            console.log(err)
-            res.status(400).send(err)
-        })
+        Team.find()
+            .sort(sort)
+            .select(fields)
+            .limit(limit)
+            .skip(skip)
+            .then(teams => res.json({ results: teams }))
+            .catch(err => {
+                console.log(err)
+                res.status(400).send(err)
+            })
+    },
+
+    create(req, res) {
+        Team.create(req.body)
+            .then(team => res.redirect(`/teams/${team._id}`))
+            .catch(err => res.status(400).send(err))
+    },
+
+    show(req, res) {
+        const _id = req.params.id
+        const { fields } = req.queries
+
+        Team.findById(_id)
+            .select(fields)
+            .then(team => {
+                if (!team) {
+                    return res.status(404)
+                        .send('No team with this ID was found')
+                }
+                res.json(team)
+            })
+            .catch(err => res.status(400).send(err))
+    }
 }
-
-const create = (req, res) => {
-    Team.create(req.body)
-        .then(team => res.redirect(`/teams/${team._id}`))
-        .catch(err => res.status(400).send(err))
-}
-
-const show = (req, res) => {
-    const _id = req.params.id
-    const { fields } = req.queries
-
-    Team.findById(_id)
-        .select(fields)
-        .then(team => {
-            if (!team) {
-                return res.status(404)
-                    .send('No team with this ID was found')
-            }
-            res.json(team)
-        })
-        .catch(err => res.status(400).send(err))
-}
-
-module.exports = { index, create, show }
